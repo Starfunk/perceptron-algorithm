dc75f1527ed10e69bb179206e3ccf280423b5f8e
Maximilian Kahn <38166078+Starfunk@users.noreply.github.com>
2018-09-09 23:15:11 -0700

    Update perceptron-algorithm-maximilian.py

--- perceptron-algorithm-maximilian.py
+++ perceptron-algorithm-maximilian.py
@@ -16,11 +16,8 @@ N = 10000
 #The range is from 0 to the mean value of the x and y data points.
 #TODO: Improve the range of the initial random numbers
 a = random.uniform(0,df.mean()[0])
-
 b = random.uniform(0, df.mean()[1])
-
 c = random.uniform(0, df.mean()[1])
-
 print("The initial random line is: "+str(round(a,3))+" * X1 + "+str(round(b,3))+" * X2 + "+str(round(c,3)))	
 
 #x and y are the coordinates of the point. a,b, and c are the values of the line. 
@@ -31,29 +28,24 @@ def classify(x,y,label,a,b,c):
 			return True
 		else:
 			return False
-			
 	elif value < 0:
 		if label == 1:
 			return True
 		else:
 			return False
-			
 	elif value == 0:
 		return False
 
 def update_line(x,y,label,a,b,c):
 	flag = classify(x,y,label,a,b,c)
-	
 	if flag == False and label == 1:
 		a = a - learning_rate * x
 		b = b - learning_rate * y
 		c = c - learning_rate
-		
 	elif flag == False and label == 0:
 		a = a + learning_rate * x
 		b = b + learning_rate * y
 		c = c + learning_rate
-		
 	return (a, b, c)
 
 #The learning process:  
@@ -63,9 +55,7 @@ for i in range(N):
 	x = points[0]
 	y = points[1]
 	label = points[2]
-	
 	updated_coefficients = update_line(x,y,label,a,b,c)
-
 	a = updated_coefficients[0]
 	b = updated_coefficients[1]
 	c = updated_coefficients[2]
@@ -73,8 +63,6 @@ for i in range(N):
 print()	
 print("After running " + str(N) + " iterations, the trained classifier is: "
 	+str(round(a,3))+" * X1 + "+str(round(b,3))+" * X2 + "+str(round(c,3)))	
-	
-
 #Plotting the points!
 x = df.iloc[:,0]
 y = df.iloc[:,1]
@@ -83,9 +71,4 @@ color=['red' if l == 1 else 'blue' for l in label]
 ax = plt.axes()
 plt.scatter(x,y,color=color)
 ax.plot(x,-(a/b)*x - (c/b),color='black')
-
 plt.show()
-
-
-
-
